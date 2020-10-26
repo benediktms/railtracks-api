63c2e33400b3fe04938de4130dddf691d80a3067
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-10-26 19:08:06 +0000

    fixed type in sessions controller

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params['User']['email']).try(:authenticate, params['user']['password'])
+    user = User.find_by(email: params['user']['email']).try(:authenticate, params['user']['password'])
 
     if user
       session[:user_id] = user.id
