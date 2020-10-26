2d0afce62d6ddd04e43c4616e01d304529411936
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-10-26 07:42:22 +0000

    removed nav menu that does not belong in here

--- frontend/components/navbar/Navbar.tsx
+++ frontend/components/navbar/Navbar.tsx
@@ -17,33 +17,6 @@ export const Navbar = () => {
   return (
     <nav>
       <Flex py={5} px={5} justify='flex-end'>
-        <Box mr={2}>
-          <Menu>
-            {/* @ts-ignore */}
-            <MenuButton as={Button} rightIcon='chevron-down'>
-              Info
-            </MenuButton>
-            <MenuList>
-              {/* @ts-ignore */}
-              <MenuItem as='a' href='https://github.com/benediktms'>
-                My GitHub Repo
-              </MenuItem>
-              {/* @ts-ignore */}
-              <MenuItem
-                as='a'
-                /* @ts-ignore */
-                href='https://www.linkedin.com/in/benedikt-schnatterbeck-813899b5/'
-              >
-                LinkedIn
-              </MenuItem>
-              {/* @ts-ignore */}
-              <MenuItem as='a' href='#'>
-                My CV
-              </MenuItem>
-              <MenuItem>Delete</MenuItem>
-            </MenuList>
-          </Menu>
-        </Box>
         <IconButton
           aria-label='toggle color mode'
           icon={colorMode === 'light' ? 'moon' : 'sun'}
