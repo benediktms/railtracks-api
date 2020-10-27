14c9105b90d0664977cf37d4cf59d7cbe9317af3
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-10-27 08:55:47 +0000

    added initial user model test

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'should create a new User instance' do
+    @user = User.new(
+      email: 'benediktms@hey.com',
+      password: 'test12345',
+      password_confirmation: 'test12345'
+    )
+    expect(@user).to be_a_new(User)
+  end
 end
