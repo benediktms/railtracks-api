4e48d1ba5e2b40524ab3395a72b6f9a2cb871900
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-10-27 08:18:56 +0000

    adding pending descriptions to specs

--- backend/spec/requests/registrations_request_spec.rb
+++ backend/spec/requests/registrations_request_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "Registrations", type: :request do
-
+RSpec.describe 'Registrations', type: :request do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
-
