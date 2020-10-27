5e64a631c14620ae789ca37f82e69c2493469f15
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-10-27 15:45:25 +0000

    added more validations on user model, added index to user email

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 8 }
+
   has_secure_password
-  validates :email, presence: true, uniqueness: true
 end
--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -3,11 +3,19 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   it 'should create a new User instance' do
-    @user = User.new(
-      email: 'benediktms@hey.com',
-      password: 'test12345',
-      password_confirmation: 'test12345'
-    )
-    expect(@user).to be_a_new(User)
+    user = User.new
+    expect(user).to be_a_new(User)
   end
+
+  it 'should have a valid email' do
+    user = User.new(email: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'should have a valid password' do
+    user = User.new(password: 'hi')
+    expect(user).to_not be_valid
+  end
+
+  it 'should have a valid password'
 end
