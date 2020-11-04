e5b8e238d014b92bcfe0d9c3a75815784f8238f1
Benedikt Schnatterbeck <benediktms@hey.com>
2020-11-04 00:48:39 +0000

    server rendering a 500 error on registration. investigating...

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,16 +1,14 @@
 class Api::V1::RegistrationsController < ApplicationController
   def create
-    user = User.create!(
-      email: params['user']['email'],
-      password: params['user']['password'],
-      password_confirmation: params['user']['password_confirmation']
-    )
-    if user
+    user =
+      User.new(
+        email: params['user']['email'],
+        password: params['user']['password'],
+        password_confirmation: params['user']['password_confirmation']
+      )
+    if user.save
       session[:user_id] = user.id
-      render json: {
-        status: created,
-        user: user
-      }
+      render json: { status: creatd, user: user }
     else
       render json: { status: 500 }
     end
